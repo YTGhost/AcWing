512e1d027df99bbff304857526f3130ceae7776d
YTGhost <283304489@qq.com>
2020-03-23 21:40:29 +0800

    add 完全背包问题 2020/03/23

--- Algorithm Basics/Char5/3. 完全背包问题(一维优化).cpp
+++ Algorithm Basics/Char5/3. 完全背包问题(一维优化).cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 const int N = 1010;
-int n, m;
+int n,m;
+int v[N], w[N];
 int f[N];
-int v[N], w[N]; 
 
 int main()
 {
     cin >> n >> m;
-    
-    for(int i = 1; i<=n; i++) cin >> v[i] >> w[i];
+    for(int i = 1; i <= n; i++) cin >> v[i] >> w[i];
     
     for(int i = 1; i <= n; i++)
-        for(int j = m; j >= v[i]; j--)
+        for(int j = v[i]; j <= m; j++)
             f[j] = max(f[j], f[j-v[i]] + w[i]);
     cout << f[m] << endl;
-    
     return 0;
 }
---
+++ Algorithm Basics/Char5/3. 完全背包问题(朴素).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1010;
+int n,m;
+int v[N], w[N];
+int f[N][N];
+
+int main()
+{
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin >> v[i] >> w[i];
+    
+    for(int i = 1; i <= n; i++)
+        for(int j = 0; j <= m; j++)
+            for(int k = 0; k*v[i] <= j; k++)
+                f[i][j] = max(f[i][j], f[i-1][j - k*v[i]] + k*w[i]);
+    
+    cout << f[n][m] << endl;
+    return 0;
+}
