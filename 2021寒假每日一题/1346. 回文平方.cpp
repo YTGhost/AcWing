fd147faf63ac97ba390d7fdd700875bb9b749270
YTGhost <283304489@qq.com>
2021-01-13 22:26:26 +0800

    update

--- 2021寒假每日一题/1346. 回文平方.cpp
+++ 2021寒假每日一题/1346. 回文平方.cpp
@@ -19,7 +19,7 @@ string changeToB(int num, int b)
 bool check(string num)
 {
     for(int i = 0, j = num.size()-1; i < j; i++, j--)
-         if(num[i] != num[j]) return false;
+        if(num[i] != num[j]) return false;
     return true;
 }
 
