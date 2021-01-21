467224e8c07f10eb4f0fd56a6b2405b97690d05b
Deng Liang <283304489@qq.com>
2021-01-21 20:41:35 +0800

    add 754 2021/01/21

---
+++ 2021寒假每日一题/754. 平方矩阵 II.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin >> n && n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+                cout << abs(i - j) + 1 << " ";
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
