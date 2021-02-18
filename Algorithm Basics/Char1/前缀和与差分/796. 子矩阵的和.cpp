08c8f115a592e57ee6ded297ad1b2c4f2e2b7645
Deng Liang <283304489@qq.com>
2021-02-18 23:27:05 +0800

    add 796 2021/02/18

---
+++ Algorithm Basics/Char1/前缀和与差分/796. 子矩阵的和.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1010;
+int a[N][N];
+
+int main()
+{
+    int n, m, q;
+    cin >> n >> m >> q;
+    
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+        {
+            int x;
+            cin >> x;
+            a[i][j] = x + a[i-1][j] + a[i][j-1] - a[i-1][j-1];
+        }
+    while(q--)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << a[x2][y2] - a[x2][y1-1] - a[x1-1][y2] + a[x1-1][y1-1] << endl;
+    }
+    return 0;
+}
